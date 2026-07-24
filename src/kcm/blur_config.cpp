c3dd9938dc9b243af9672ecb18c92682cb766142
Harnocca <hauseowo@outlook.com>
2026-07-24 17:46:29 +0800

    i18n: add zh_CN translation support via ki18n_install

--- src/kcm/blur_config.cpp
+++ src/kcm/blur_config.cpp
@@ -14,6 +14,7 @@
 #include "kwineffects_interface.h"
 #include "settings.hpp"
 
+#include <KLocalizedString>
 #include <QFileDialog>
 #include <QPushButton>
 
@@ -24,6 +25,7 @@ K_PLUGIN_CLASS(BlurEffectConfig)
 BlurEffectConfig::BlurEffectConfig(QObject *parent, const KPluginMetaData &data)
     : KCModule(parent, data)
 {
+    KLocalizedString::setApplicationDomain("kwin_better_blur_dx");
     ui.setupUi(widget());
     BlurConfig::instance("kwinrc");
     addConfig(BlurConfig::self(), widget());
