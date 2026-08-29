0aa23c54cd1fa7c3df2baffcace5ee551ccb85fa
Xarblu <xarblu@protonmail.com>
2026-08-29 21:25:36 +0200

    window: simplify
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/window.cpp
+++ src/window.cpp
@@ -353,11 +353,7 @@ void BBDX::Window::reconfigure() {
     m_blurMenus = m_windowManager->blurMenus();
     m_blurDocks = m_windowManager->blurDocks();
 
-    if (shouldForceBlur()) {
-        m_shouldForceBlur = true;
-    } else {
-        m_shouldForceBlur = false;
-    }
+    m_shouldForceBlur = shouldForceBlur();
 
     m_userBorderRadius = m_windowManager->userBorderRadius();
 
